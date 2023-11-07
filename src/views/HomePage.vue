fd63b4e3f24a0c5b086bd869823c08d5393293a5
Fernando Sánchez Noguez <fer_noguez890@outlook.com>
2023-11-06 21:44:41 -0600

    Primeras Interfaces
    Creación de las primeras interfaces

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,140 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Iniciar Sesión</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding">
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
+      <ion-card class="ion-card-small">
+        <!-- Agregamos una imagen arriba del formulario -->
+        <img
+          src="https://png.pngtree.com/png-vector/20191110/ourlarge/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
+          alt="Login Image" class="login-image" />
+        <ion-card-content>
+          <ion-list>
+            <ion-item class="rounded-item">
+              <ion-label class="custom-label">Usuario</ion-label>
+              <ion-input v-model="usuario" type="text" required class="custom-input"></ion-input>
+            </ion-item>
+
+            <ion-item class="rounded-item">
+              <ion-label class="custom-label">Contraseña</ion-label>
+              <ion-input v-model="contrasena" type="password" required class="custom-input"></ion-input>
+            </ion-item>
+
+            <ion-button expand="full" @click="iniciarSesion" class="custom-button">Iniciar Sesión</ion-button>
+
+            <ion-button expand="full" @click="registrarUsuario" class="custom-button">Registrate Aquí</ion-button>
+
+            <ion-toast v-model="mostrarToastError" message="Usuario o contraseña incorrectos." position="top"
+              color="danger" duration="5000"></ion-toast>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import { IonInput, IonButton, IonToast, IonPage } from '@ionic/vue';
+import { ref } from 'vue';
+
+export default {
+  components: { IonInput, IonButton, IonToast, IonPage },
+  name: 'Home',
+  data() {
+    return {
+      usuario: '',
+      contrasena: '',
+      mostrarToastError: false,
+    };
+  },
+  methods: {
+    iniciarSesion() {
+      let errorMessage = "";
+
+      if (!this.usuario) {
+        errorMessage += "Por favor, ingrese el usuario.\n";
+      }
+
+      if (!this.contrasena) {
+        errorMessage += "Por favor, ingrese la contraseña.\n";
+      }
+
+      if (errorMessage) {
+        // Muestra el mensaje de error
+        alert("Error: \n" + errorMessage);
+      } else {
+        // lógica para verificar las credenciales del usuario.
+      }
+    },
+
+    registrarUsuario() {
+      this.$router.push("/registro")
+    }
+  },
+};
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.ion-card-small {
+  max-width: 400px;
+  margin: 0 auto;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+ion-card {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-button {
+  margin-top: 10px;
 }
 
-#container a {
-  text-decoration: none;
+ion-item.rounded-item {
+  border-radius: 10px;
+  margin-bottom: 10px;
 }
-</style>
+
+ion-label.custom-label {
+  width: 40%;
+  text-align: right;
+  margin-right: 20px;
+}
+
+ion-input.custom-input {
+  width: 60%;
+  border-radius: 10px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border: 2px solid #ccc;
+}
+
+.custom-button {
+  border-radius: 10px;
+  --color: #F5FCCD;
+  --background: #034561;
+  font-size: 14px;
+  padding: 8px 16px;
+}
+
+.login-image {
+  display: block;
+  margin: 0 auto;
+  max-width: 150px;
+  height: 150px;
+  border-radius: 100px;
+}
+
+ion-item {
+  --border-width: 0;
+  /* Elimina el borde */
+  --inner-border-width: 0;
+  /* Elimina el borde interno */
+}</style>
---
+++ src/views/RutasPage.vue
@@ -0,0 +1,219 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Crear Ruta</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <ion-card class="ion-card-small">
+                <ion-card-content>
+                    <ion-list>
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Lugar de Origen</ion-label>
+                            <ion-select label="Lugar de Origen" label-placement="fixed" v-model="lugarSalida">
+                                <ion-select-option value="Origen1">Origen 1</ion-select-option>
+                                <ion-select-option value="Origen2">Origen 2</ion-select-option>
+                            </ion-select>
+                        </ion-item>
+
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Lugar de Destino</ion-label>
+                            <ion-select label="Lugar de Destino" label-placement="fixed" v-model="lugarDestino">
+                                <ion-select-option value="Destino1">Destino 1</ion-select-option>
+                                <ion-select-option value="Destino2">Destino 2</ion-select-option>
+                            </ion-select>
+                        </ion-item>
+
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Número de Pasajeros</ion-label>
+                            <ion-button fill="solid" color="primary" @click="decrementPasajeros">
+                                <ion-icon name="remove-circle-outline" size="large"></ion-icon>
+                            </ion-button>
+                            <ion-input type="number" v-model="luagresDisponibles" min="0" max="99" step="1"
+                                class="custom-input custom-input-border-color-1"></ion-input>
+                            <ion-button fill="solid" color="primary" @click="incrementPasajeros">
+                                <ion-icon name="add-circle-outline" size="large"></ion-icon>
+                            </ion-button>
+                        </ion-item>
+
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Fecha y Hora de Salida</ion-label>
+                            <ion-datetime-button datetime="datetime" v-model="fechaSalida"
+                                class="custom-button"></ion-datetime-button>
+                            <ion-modal :keep-contents-mounted="true">
+                                <ion-datetime id="datetime"></ion-datetime>
+                            </ion-modal>
+                        </ion-item>
+
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Costo</ion-label>
+                            <ion-input type="number" v-model="costo" min="0" step="0.01"
+                                class="custom-input custom-input-border-color-2"></ion-input>
+                        </ion-item>
+
+                        <ion-item class="rounded-item">
+                            <ion-label class="custom-label">Selecciona la Unidad</ion-label>
+                            <ion-label class="custom-label">Selecciona la Unidad</ion-label>
+                            <ion-select label="Selecciona la Unidad" label-placement="fixed" v-model="idUnidad">
+                                <ion-select-option v-for="unidad in unidades" :key="unidad.id" :value="unidad.id">
+                                    {{ unidad.nombre }}
+                                </ion-select-option>
+                            </ion-select>
+                        </ion-item>
+
+                        <ion-button expand="full" @click="crearRuta" class="custom-button">Crear Ruta</ion-button>
+                    </ion-list>
+                </ion-card-content>
+            </ion-card>
+        </ion-content>
+    </ion-page>
+</template>
+  
+<script>
+import { IonDatetime, IonDatetimeButton, IonModal, IonInput, IonCard, IonCardContent, IonButton, IonItem, IonList, IonTitle, IonToolbar, IonHeader, IonLabel, IonContent, IonPage, IonSelect, IonSelectOption } from '@ionic/vue';
+import { ref } from 'vue';
+
+export default {
+    components: { IonDatetime, IonDatetimeButton, IonModal, IonInput, IonCard, IonCardContent, IonButton, IonItem, IonList, IonTitle, IonToolbar, IonHeader, IonLabel, IonContent, IonPage, IonSelect, IonSelectOption },
+    name: 'RegistroRutas',
+    data() {
+        return {
+            lugarSalida: '',
+            lugarDestino: '',
+            luagresDisponibles: 0,
+            fechaSalida: '',
+            costo: 0,
+            idUsuario: '',
+            idUnidad: '',
+            estatus: '',
+            unidades: [],
+
+        };
+    },
+    onMounted() {
+        this.cargarUnidades(); // Llama a la función para cargar las unidades
+    },
+    methods: {
+        decrementPasajeros() {
+            if (this.luagresDisponibles > 0) {
+                this.luagresDisponibles--;
+            }
+        },
+        incrementPasajeros() {
+            this.luagresDisponibles++;
+        },
+        async cargarUnidades() {
+            // Aquí debes realizar una petición HTTP para obtener las unidades de la API
+            try {
+                const response = await fetch("URL_DE_TU_API/unidades");
+                if (response.ok) {
+                    // Si la respuesta es exitosa, convierte los datos JSON
+                    const data = await response.json();
+                    this.unidades = data; // Almacena las unidades en la variable "unidades"
+                } else {
+                    console.error("Error al obtener las unidades");
+                }
+            } catch (error) {
+                console.error("Error de red:", error);
+            }
+        },
+        crearRuta() {
+            let errorMessage = "";
+
+            if (!this.lugarSalida) {
+                errorMessage += "Por favor, selecciona el lugar de salida.\n";
+            }
+
+            if (!this.lugarDestino) {
+                errorMessage += "Por favor, ingresa el lugar de destino.\n";
+            }
+
+            if (!this.luagresDisponibles) {
+                errorMessage += "Por favor, ingresa el numro de lugares. \n"
+            }
+
+            if (!this.fechaSalida) {
+                errorMessage += "Por favor, ingrese la hora y fecha de salida. \n"
+            }
+
+            if (!this.costo) {
+                errorMessage += "Por favor, ingrese el costo por pasaje."
+            }
+
+            if (errorMessage) {
+                // Muestra el mensaje de error
+
+                alert("Error: \n" + errorMessage);
+            } else {
+                alert("Correcto")
+            }
+        },
+    },
+};
+</script>
+  
+<style scoped>
+.ion-card-small {
+    max-width: 700px;
+    margin: 0 auto;
+}
+
+ion-card {
+    margin: 10px;
+    padding: 10px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+
+ion-button {
+    margin-top: 10px;
+}
+
+ion-item.rounded-item {
+    border-radius: 10px;
+    margin-bottom: 10px;
+}
+
+ion-label.custom-label {
+    width: 40%;
+    text-align: right;
+    margin-right: 20px;
+}
+
+ion-input.custom-input {
+    width: 60%;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: #f0f0f0;
+    border: 2px solid #ccc;
+}
+
+ion-input.custom-input-border-color-1 {
+    border: 2px solid #F5FCCD;
+}
+
+ion-input.custom-input-border-color-2 {
+    border: 2px solid #4FB783;
+}
+
+.custom-button {
+    border-radius: 10px;
+    --color: #F5FCCD;
+    --background: #034561;
+    font-size: 14px;
+    padding: 8px 16px;
+}
+
+ion-item {
+    --border-width: 0;
+    /* Elimina el borde */
+    --inner-border-width: 0;
+    /* Elimina el borde interno */
+}
+
+ion-icon {
+    color: black;
+}
+</style>
+  
