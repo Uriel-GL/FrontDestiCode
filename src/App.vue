db8d37ab9d9c6e9ce2219bac59bbb1386caddcf2
Uriel Gonzalez Lugo <gonzalezlugou@gmail.com>
2023-11-09 11:12:31 -0600

    Pequeños cambios con el diseño del App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>DestiCode</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-router-outlet />
+    </ion-content>
+    
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
+
+<style scoped>
+
+
+</style>
